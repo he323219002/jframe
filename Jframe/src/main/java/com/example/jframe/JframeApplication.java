ed6d1e0c620314de5a87fa508831f40b2fd3bb55
Jimmy <Jimmy>
2022-12-17 15:32:44 +0800

    项目初始化

---
+++ Jframe/src/main/java/com/example/jframe/JframeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JframeApplication.class, args);
+    }
+
+}
