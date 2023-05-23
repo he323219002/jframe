c25d7f8a46561ed1a00a953437205600a461bd15
Jimmy <Jimmy>
2023-05-23 22:05:54 +0800

    ignore文件修改

---
+++ dep/common/src/main/java/com/jframe/base/Dto.java
@@ -0,0 +1,16 @@
+package com.jframe.base;
+
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @Author: Jimmy He
+ * @Date: 2023/4/6 22:15
+ * @Description: dto基类
+ */
+public abstract class Dto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -1L;
+}
---
+++ dep/common/src/main/java/com/jframe/base/Entity.java
@@ -0,0 +1,9 @@
+package com.jframe.base;
+
+/**
+ * @Author: Jimmy He
+ * @Date: 2023/4/6 22:24
+ * @Description: entity基类
+ */
+public abstract class Entity {
+}
